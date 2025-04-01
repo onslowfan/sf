9ef5176d14aafc61df5401e5bdf7bb20d029a5d0
raoul <rreyes@advisen.com>
2025-04-01 14:48:03 +0800

    test

---
+++ test.sql
@@ -0,0 +1,10 @@
+use database testing;
+create or replace Procedure X2()
+RETURNS VARCHAR NOT NULL
+LANGUAGE SQL
+    as
+    begin
+    create table TESTING.Test.abc as select * from TESTING.TEST.VS where 1=0;
+    return 'wah';
+    end;
+
